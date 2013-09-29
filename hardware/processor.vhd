1b48adea003f77cafcea0e20eaff882661df194b
Runar B. Olsen <runar.b.olsen@gmail.com>
2013-09-29 16:58:53 +0200

    Fixed formatting, linked jump signal, added comments.

--- hardware/processor.vhd
+++ hardware/processor.vhd
@@ -1,60 +1,35 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Processor implementation
 -- 
--- Create Date:    09:25:34 09/16/2013 
--- Design Name: 
--- Module Name:    processor - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
 --
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+
+library ieee;
+use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 library work;
 use work.mips_constant_pkg.all;
 
-entity processor is
-	
+-- Processor interface, as defined by the provided toplevel implementation
+entity processor is	
 	generic  (
 		MEM_ADDR_BUS	: integer	:= 32;
 		MEM_DATA_BUS	: integer	:= 32 );
-
 	port ( 
-		clk               : in STD_LOGIC;
-		reset					: in STD_LOGIC;
-		processor_enable	: in  STD_LOGIC;
-		imem_address 		: out  STD_LOGIC_VECTOR (MEM_ADDR_BUS-1 downto 0);
-		imem_data_in 		: in  STD_LOGIC_VECTOR (MEM_DATA_BUS-1 downto 0);
-		dmem_data_in 		: in  STD_LOGIC_VECTOR (MEM_DATA_BUS-1 downto 0);
-		dmem_address 		: out  STD_LOGIC_VECTOR (MEM_ADDR_BUS-1 downto 0);
-		dmem_address_wr	: out  STD_LOGIC_VECTOR (MEM_ADDR_BUS-1 downto 0);
-		dmem_data_out		: out  STD_LOGIC_VECTOR (MEM_DATA_BUS-1 downto 0);
-		dmem_write_enable	: out  STD_LOGIC
+		clk               : in std_logic;
+		reset					: in std_logic;
+		processor_enable	: in  std_logic;
+		imem_address 		: out  std_logic_vector (MEM_ADDR_BUS-1 downto 0);
+		imem_data_in 		: in  std_logic_vector (MEM_DATA_BUS-1 downto 0);
+		dmem_data_in 		: in  std_logic_vector (MEM_DATA_BUS-1 downto 0);
+		dmem_address 		: out  std_logic_vector (MEM_ADDR_BUS-1 downto 0);
+		dmem_address_wr	: out  std_logic_vector (MEM_ADDR_BUS-1 downto 0);
+		dmem_data_out		: out  std_logic_vector (MEM_DATA_BUS-1 downto 0);
+		dmem_write_enable	: out  std_logic
 	);
 end processor;
 
 architecture Behavioral of processor is
-
 	component control_unit is
 		port (
 			clk        : in std_logic;
@@ -68,75 +43,66 @@ architecture Behavioral of processor is
 			mem_write  : out std_logic;
 			alu_src    : out std_logic;
 			reg_write  : out std_logic;
-			pc_latch   : out std_logic
+			pc_latch   : out std_logic;
+			jump       : out std_logic
 		);
 	end component;
 	
 	component alu_control is
 		port (
-			func: in std_logic_vector(5 downto 0);
-			ALU_OP : in ALU_OP;
-			ALU_INPUT : out ALU_INPUT
+			func      : in std_logic_vector(5 downto 0);
+			alu_op    : in ALU_OP;
+			alu_input : out ALU_INPUT
 		);
 	end component;
 
 	component alu is
 		generic (N : natural := 32);
 		port (
-			X			: in STD_LOGIC_VECTOR(N-1 downto 0);
-			Y			: in STD_LOGIC_VECTOR(N-1 downto 0);
-			ALU_IN	: in ALU_INPUT;
-			R			: out STD_LOGIC_VECTOR(N-1 downto 0);
-			FLAGS		: out ALU_FLAGS
+			x       : in std_logic_vector(N-1 downto 0);
+			y       : in std_logic_vector(N-1 downto 0);
+			alu_in  : in ALU_INPUT;
+			r       : out std_logic_vector(N-1 downto 0);
+			flags   : out ALU_FLAGS
 		);
 	end component;
 		
 	component register_file is
 		port (
-			CLK 			:	in	STD_LOGIC;				
-			RESET			:	in	STD_LOGIC;				
-			RW				:	in	STD_LOGIC;				
-			RS_ADDR 		:	in	STD_LOGIC_VECTOR (RADDR_BUS-1 downto 0); 
-			RT_ADDR 		:	in	STD_LOGIC_VECTOR (RADDR_BUS-1 downto 0); 
-			RD_ADDR 		:	in	STD_LOGIC_VECTOR (RADDR_BUS-1 downto 0);
-			WRITE_DATA	:	in	STD_LOGIC_VECTOR (DDATA_BUS-1 downto 0); 
-			RS				:	out STD_LOGIC_VECTOR (DDATA_BUS-1 downto 0);
-			RT				:	out STD_LOGIC_VECTOR (DDATA_BUS-1 downto 0)
+			clk        : in std_logic;				
+			reset      : in std_logic;				
+			rw	       : in std_logic;				
+			rs_addr    : in std_logic_vector (RADDR_BUS-1 downto 0); 
+			rt_addr    : in std_logic_vector (RADDR_BUS-1 downto 0); 
+			rd_addr    : in std_logic_vector (RADDR_BUS-1 downto 0);
+			write_data : in std_logic_vector (DDATA_BUS-1 downto 0); 
+			rs         : out std_logic_vector (DDATA_BUS-1 downto 0);
+			rt         : out std_logic_vector (DDATA_BUS-1 downto 0)
 		);
 	end component;
 	
 	component sign_extend is
 		port (
-			A : in std_logic_vector(15 downto 0);
-			R : out std_logic_vector(31 downto 0)
+			a : in std_logic_vector(15 downto 0);
+			r : out std_logic_vector(31 downto 0)
 		);
 	end component;
---	
---	component mux is
---		generic ( N: natural := 32);
---		port (
---			S : in std_logic;
---			A : in std_logic_vector(N-1 downto 0);
---			B : in std_logic_vector(N-1 downto 0);
---			R : out std_logic_vector(N-1 downto 0)
---		);
---	end component;
---	
+
 	component adder is 
 		generic (N: natural := 32);
 		port (
-			X	   : in	STD_LOGIC_VECTOR(N-1 downto 0);
-			Y	   : in	STD_LOGIC_VECTOR(N-1 downto 0);
-			CIN	: in	STD_LOGIC;
-			COUT	: out	STD_LOGIC;
-			R	   : out	STD_LOGIC_VECTOR(N-1 downto 0)
+			x    : in std_logic_vector(N-1 downto 0);
+			y    : in std_logic_vector(N-1 downto 0);
+			cin	 : in std_logic;
+			cout : out std_logic;
+			r    : out std_logic_vector(N-1 downto 0)
 		);
 	end component;
 	
 	component shiftleft is
 		port(
-			A : in std_logic_vector (31 downto 0);
-			R : out std_logic_vector (31 downto 0)
+			a : in std_logic_vector (31 downto 0);
+			r : out std_logic_vector (31 downto 0)
 		);
 	end component;
 	
@@ -154,8 +120,7 @@ architecture Behavioral of processor is
 		
 	-- Mux register input
 	signal reg_write_mux_out  : std_logic_vector (4 downto 0);
-	signal data_write_mux_out : std_logic_vector (31 downto 0);
-	
+	signal data_write_mux_out : std_logic_vector (31 downto 0);	
 	signal alu_in_mux : std_logic_vector (31 downto 0);
 	
 	-- Register output
@@ -171,18 +136,18 @@ architecture Behavioral of processor is
 	signal flags   : ALU_FLAGS;
 
 	-- Program Counter signals
-	signal PC      : std_logic_vector (31 downto 0) := "00000000000000000000000000000000";
-	signal PC_INC  : std_logic_vector (31 downto 0);
-	signal PC_JUMP : std_logic_vector (31 downto 0);
-	signal PC_NEXT : std_logic_vector (31 downto 0);
-	signal PC_INC2 : std_logic_vector (31 downto 0);
+	signal PC        : std_logic_vector (31 downto 0) := "00000000000000000000000000000000";
+	signal PC_INC    : std_logic_vector (31 downto 0); -- PC + 4
+	signal PC_JUMP   : std_logic_vector (31 downto 0); -- Jump target
+	signal PC_BRANCH : std_logic_vector (31 downto 0); -- Branch target
+	signal PC_NEXT   : std_logic_vector (31 downto 0); -- Next PC value
 	
 	signal sign_extended : std_logic_vector (31 downto 0);
 	signal shifted       : std_logic_vector (31 downto 0) := "00000000000000000000000000001000";
 	signal jump          : std_logic;
 
 begin
-
+	-- Control unit
 	control : control_unit port map(
 		clk        => internal_clk,
 		reset      => reset,
@@ -195,9 +160,12 @@ begin
 		mem_write  => mem_write,
 		alu_src    => alu_src,
 		reg_write  => reg_write,
-		pc_latch   => pc_latch
+		pc_latch   => pc_latch,
+		jump       => jump
 	);
 
+	-- Clock enable, while processor_enable is 1 internal_clk mirrors clk
+    -- When processor_enable is 0 internal_clk is 0 aswell
 	clk_enable: process (clk, processor_enable)
 	begin
 		if processor_enable = '1' then
@@ -207,64 +175,81 @@ begin
 		end if;
 	end process;
 
+	-- PC signal is always hooked to the instruction memory address
+	imem_address <= PC;
+
+	-- Constant added that takes PC as it's X input and the contant 4 as Y.
+    -- Result is placed in PC_INC which is the next PC given no branch or jump
 	add4: adder port map(
-		X => PC,
-		Y => "00000000000000000000000000000100",
-		CIN => '0',
-		R => PC_INC
+		x   => PC,
+		y   => "00000000000000000000000000000100", -- 4
+		cin => '0',
+		r   => PC_INC
 	);
+
+	-- Branch adder. Adds the sign extended shifted low 16 bits of the instruction to PC_INC 
+    -- in order to generate the potential branch target PC_BRANCH
+	add_jump: adder port map(
+		x   => PC_INC,
+		y   => shifted,
+		cin => '0',
+		r   => PC_BRANCH
+	);
+
+	-- TODO PC_JUMP
 	
-	pc_latch1: process (pc_latch, PC_INC)
+	-- PC next mux, combines the jump and branch mux	
+	pc_mux: process (jump, branch, PC_JUMP, PC_BRANCH, PC_INC)
+	begin
+		if jump = '1' then
+			PC_NEXT <= PC_JUMP;
+		else
+			if branch = '1' and flags.Zero = '1' then
+				PC_NEXT <= PC_BRANCH;
+			else 
+				PC_NEXT <= PC_INC;
+			end if;
+		end if;
+	end process;
+		
+    -- Program counter latch. Updates the PC register on rising internal clock.
+    -- The next program counter value is located in PC_NEXT
+	pc_latch1: process (pc_latch, PC_NEXT)
 	begin
 		if rising_edge(pc_latch) then
 			PC <= PC_NEXT;
 		end if;
 	end process;
 	
-	imem_address <= PC;
-	
-	pc_mux: process (jump, PC_INC, PC_JUMP)
-	begin
-		if jump = '1' then
-			PC_NEXT <= PC_JUMP;
-		else
-			PC_NEXT <= PC_INC;
-		end if;
-	end process;
-	
-	add_jump: adder port map(
-		X => PC_INC,
-		Y => shifted,
-		CIN => '0',
-		R => PC_JUMP
-	);
-		
+	-- Signextend the low 16 bits of the instruction
 	signex: sign_extend port map(
 		A => imem_data_in(15 downto 0),
 		R => sign_extended
 	);
 	
+	-- Shift the signextended low 16 bits of the instruction two bits to the left
 	shift2: shiftleft port map(
 		A => sign_extended,
 		R => shifted
 	);
 	
+	-- ALU Control unit
 	aluctl: alu_control port map(
-		func => imem_data_in(5 downto 0),
-		ALU_OP => alu_op,
-		ALU_INPUT => alu_input
+		func      => imem_data_in(5 downto 0),
+		alu_op    => alu_op,
+		alu_input => alu_input
 	);
 
+	-- ALU
 	alu1: alu port map(
-		X => rs,
-		Y => alu_in_mux,
-		ALU_IN => alu_input,
-		R => alu_out,
-		FLAGS => flags
+		x      => rs,
+		y      => alu_in_mux,
+		alu_in => alu_input,
+		r      => alu_out,
+		flags  => flags
 	);
-	
-	jump <= flags.Zero and branch;
-	
+		
+	-- Switch which register is set as the write register
 	reg_write_mux: process(reg_dst, imem_data_in) 
 	begin
 		if reg_dst = '1' then
@@ -274,18 +259,20 @@ begin
 		end if;
 	end process;
 	
+	-- Map the register file
 	regfile: register_file port map(
-		clk => internal_clk,
-		reset => reset,
-		rw => reg_write,
-		rs_addr => imem_data_in(25 downto 21),
-		rt_addr => imem_data_in(20 downto 16),
-		rd_addr => reg_write_mux_out,
+		clk        => internal_clk,
+		reset      => reset,
+		rw         => reg_write,
+		rs_addr    => imem_data_in(25 downto 21),
+		rt_addr    => imem_data_in(20 downto 16),
+		rd_addr    => reg_write_mux_out,
 		write_data => data_write_mux_out,
-		rs => rs,
-		rt => rt
+		rs         => rs,
+		rt         => rt
 	);
 	
+	-- Switch alu input, eiter rt or sign_extended based on alu_src
 	alu_in_mux1: process(rt, sign_extended, alu_src) 
 	begin
 		if alu_src = '1' then
@@ -295,11 +282,13 @@ begin
 		end if;
 	end process;
 		
-	dmem_address <= alu_out;
-	dmem_address_wr <= alu_out;
-	dmem_data_out <= rt;
+	-- Link dmem inputs
+	dmem_address      <= alu_out;
+	dmem_address_wr   <= alu_out;
+	dmem_data_out     <= rt;
 	dmem_write_enable <= mem_write;
 	
+	-- mem_to_reg mux, selects what data is sent to the write register
 	data_out_mux: process(dmem_data_in, alu_out, mem_to_reg)
 	begin
 		if mem_to_reg = '1' then
