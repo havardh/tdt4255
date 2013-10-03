33a336cee7ee7514cdbc9deabf8c15d5d1072f2b
Runar B. Olsen <runar.b.olsen@gmail.com>
2013-10-03 11:12:01 +0200

    Cleaned project tree

--- system/system.init_bram.tcl
+++
@@ -1,8 +0,0 @@
-cd C:/Users/runarbol/Desktop/tdt4255-new/system/system
-if { [ catch { xload xmp system.xmp } result ] } {
-  exit 10
-}
-if { [catch {run init_bram} result] } {
-  exit -1
-}
-exit 0
