e9d577cffe0be590484347929490947158de36ad
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2013-10-15 14:00:46 +0200

    Updated control_unit and control_unit_tb for pipelined design.

--- system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl/control_unit_tb.vhd
+++ system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl/control_unit_tb.vhd
@@ -4,261 +4,123 @@ use ieee.std_logic_1164.all;
 library work;
 use work.mips_constant_pkg.all;
 use work.asserts.all;
- 
+use work.pipeline_types.all;
+
+
 entity control_unit_tb is
 end control_unit_tb;
- 
+
 architecture Behavior of control_unit_tb is
- 
-    -- Component Declaration for the Unit Under Test (UUT)
-    component control_unit
-        port (
-            clk : in  std_logic;
-            reset : in  std_logic;
-            opcode : in  std_logic_vector(5 downto 0);
-            enable : in std_logic;
-            reg_dst : out  std_logic;
-            branch : out  std_logic;
-            mem_read : out  std_logic;
-            mem_to_reg : out  std_logic;
-            alu_op : out  ALU_OP;
-            mem_write : out  std_logic;
-            alu_src : out  std_logic;
-            reg_write : out  std_logic;
-            jump : out  std_logic;
-            pc_latch : out std_logic;
-            link : out std_logic
-        );
-    end component;
-    
+	
+	-- Component Declaration for the Unit Under Test (UUT)
+	component control_unit
+		port (
+			opcode : in  std_logic_vector(5 downto 0);
+			ctrl_ex    : out ctrl_ex_t;
+			ctrl_m     : out ctrl_m_t;
+			ctrl_wb    : out ctrl_wb_t
+			);
+	end component;
+	
 
-    --Inputs
-    signal clk    : std_logic := '0';
-    signal reset  : std_logic := '0';
-    signal opcode : std_logic_vector(5 downto 0) := (others => '0');
-    signal enable : std_logic := '0';
+	--Inputs
+	signal opcode : std_logic_vector(5 downto 0) := (others => '0');
 
-     --outputs
-    signal reg_dst    : std_logic;
-    signal branch     : std_logic;
-    signal mem_read   : std_logic;
-    signal mem_to_reg : std_logic;
-    signal mem_write  : std_logic;
-    signal alu_src    : std_logic;
-    signal reg_write  : std_logic;
-    signal jump       : std_logic;
-    signal pc_latch   : std_logic;
-    signal link       : std_logic;
-    signal alu_op     : ALU_OP;
+	-- Outputs
+	signal ctrl_ex : ctrl_ex_t;
+	signal ctrl_m  : ctrl_m_t;
+	signal ctrl_wb : ctrl_wb_t;
 
-    -- In order to ease debugging we map the 9 control signals (excluding alu_op) to this
-    -- single vector, hence we can do a single assert per test case insted of 9
-    -- The mapping is shown at the start of the test bench below
-    signal output : std_logic_vector (9 downto 0);
+	constant ALU : std_logic_vector (5 downto 0) := "000000"; -- X"0";
+	constant LW  : std_logic_vector (5 downto 0) := "100011"; -- X"23";
+	constant SW  : std_logic_vector (5 downto 0) := "101011"; -- X"2b";
+	constant LUI : std_logic_vector (5 downto 0) := "001111"; -- X"F";
+	constant BEQ : std_logic_vector (5 downto 0) := "000100"; -- X"4";
+	constant J   : std_logic_vector (5 downto 0) := "000010"; -- X"2";
+	--constant JAL : std_logic_vector (5 downto 0) := X"000011";
 
-    -- Clock period definitions
-    constant clk_period : time := 10 ns;
+	signal output : std_logic_vector(6 downto 0);
 
+	
 begin
- 
-    -- Instantiate the Unit Under Test (UUT)
-    uut: control_unit 
-        port map (
-            clk => clk,
-            reset => reset,
-            opcode => opcode,
-            enable => enable,
-            reg_dst => reg_dst,
-            branch => branch,
-            mem_read => mem_read,
-            mem_to_reg => mem_to_reg,
-            alu_op => alu_op,
-            mem_write => mem_write,
-            alu_src => alu_src,
-            reg_write => reg_write,
-            jump => jump,
-            pc_latch => pc_latch,
-            link => link
-        );
+	
+	-- Instantiate the Unit Under Test (UUT)
+	uut: control_unit 
+		port map (
+			opcode => opcode,
 
-    -- mapping output vector
-    output(9) <= link;
-    output(8) <= pc_latch;
-    output(7) <= reg_dst;
-    output(6) <= branch;
-    output(5) <= mem_read;
-    output(4) <= mem_to_reg;
-    output(3) <= mem_write;
-    output(2) <= alu_src;
-    output(1) <= reg_write;
-    output(0) <= jump;
+			ctrl_ex => ctrl_ex,
+			ctrl_m => ctrl_m,
+			ctrl_wb => ctrl_wb
+		);
 
+	output(6) <= ctrl_ex.reg_dst;
+	output(5) <= ctrl_ex.alu_src;
+	output(4) <= ctrl_m.jump;
+	output(3) <= ctrl_m.branch;
+	output(2) <= ctrl_m.mem_write;
+	output(1) <= ctrl_wb.mem_to_reg;
+	output(0) <= ctrl_wb.reg_write;
+	
+	--output(1) => ctrl_ex.alu_op;
+	
+	-- Stimulus process
+	stim_proc: process
+	begin        
+		wait for 10 ns;
+		
+		-- Alu
+		opcode <= ALU;
+		wait for 1 ns;
+		assertEqual(output, "1000001");
+		assert(ctrl_ex.alu_op = ALUOP_FUNC)
+			report "alu_op should be ALUOP_FUNC"
+			severity warning;
+		
+		-- Load word
+		opcode <= LW;
+		wait for 1 ns;
+		assertEqual(output, "0100011");
+		assert(ctrl_ex.alu_op = ALUOP_LOAD_STORE)
+			report "alu_op should be ALUOP_LOAD_STORE"
+			severity warning;
+		
+		-- Store word
+		opcode <= SW;
+		wait for 1 ns;
+		assertEqual(output, "0100100");
+		assert(ctrl_ex.alu_op = ALUOP_LOAD_STORE)
+			report "alu_op should be ALUOP_LOAD_STORE"
+			severity warning;
 
-    -- Clock process definitions
-    clk_process :process
-    begin
-        clk <= '0';
-        wait for clk_period/2;
-        clk <= '1';
-        wait for clk_period/2;
-    end process;
- 
+		-- Load upper immideate
+		opcode <= LUI;
+		wait for 1 ns;
+		assertEqual(output, "0100001");
+		assert(ctrl_ex.alu_op = ALUOP_LDI)
+			report "alu_op should be ALUOP_LDI"
+			severity warning;
+		
+		-- Branch if equal
+		opcode <= BEQ;
+		wait for 1 ns;
+		assertEqual(output, "0001000");
+		assert(ctrl_ex.alu_op = ALUOP_BRANCH)
+			report "alu_op should be ALUOP_BRANCH"
+			severity warning;
+		
+		-- Jump
+		opcode <= J;
+		wait for 1 ns;
+		assertEqual(output, "0010000");
+		assert(ctrl_ex.alu_op = ALUOP_FUNC)
+			report "alu_op should be ALUOP_FUNC"
+			severity warning;
+		
+		-- Jump and link
+		
+		wait;
 
-    -- Stimulus process
-    stim_proc: process
-    begin        
-        enable <= '1';
-
-        -- Control unit tests:
-        -- 
-        -- The tests below all follow the same format:
-        --  - First the unit it reset, by setting reset high waiting a cyle, pulling it low and
-        --    waiting an other cycle. After this we know that the unit is in the FETCH state
-        --  - Then we set the opcode to test and wait for an other cycle
-        --  - We are now in EXECUTE and we assert that all the signals are correct.
-        --  - For normal instructions we now expect to be in FETCH and we assert that destructive 
-        --    signals are pulled low. And we set a undefined opcode.
-        --  - In the next cycle we assume all singals except pc_latch is low, as this indicates that
-        --    we're back in EXECUTE
-
-        -- R-Type instructions
-        reset <= '1';
-        wait for clk_period*1;
-        reset <= '0';
-        wait for clk_period*1;
-        assert (false) report "Testing R-Type instructions" severity note;
-
-        -- Set instruction and wait for FETCH->EXECUTE transistion
-        opcode <= "000000";
-        wait for clk_period;
-        -- Assert values and reset instruction to undefined
-        assertEqual(output, "0010000010");
-        -- Make sure all write enable and latch singals are disabled in EXECUTE->FETCH transition
-        wait for clk_period;
-        opcode <= "111111";
-        assertEqual(output, "0110000000");
-        wait for clk_period;
-        assertEqual(output, "0000000000");
-
-        -- Load word
-        reset <= '1';
-        wait for clk_period*1;
-        reset <= '0';
-        wait for clk_period*1;
-        assert (false) report "Testing load word" severity note;
-
-        -- Set intruction and wait for FETCH->EXECUTE
-        opcode <= "100011";
-        wait for clk_period;
-        -- Assert values
-        assertEqual(output, "0000110110");
-        -- Make sure values are held into the STALL state
-        wait for clk_period*1;
-        assertEqual(output, "0000110110");
-        wait for clk_period*1;
-        opcode <= "111111";
-        assertEqual(output, "0100110100");
-        wait for clk_period*1;
-        assertEqual(output, "0000000000");
-        
-
-        -- Store word
-        reset <= '1';
-        wait for clk_period*1;
-        reset <= '0';
-        wait for clk_period*1;
-        assert (false) report "Testing store word" severity note;
-
-        -- Set intruction and wait for FETCH->EXECUTE
-        opcode <= "101011";
-        wait for clk_period*1;
-        -- Assert values and reset instruction
-        assertEqual(output, "0000001100");
-        -- Make sure values are held into STALL state
-        wait for clk_period*1;
-        assertEqual(output, "0000001100");
-        wait for clk_period*1;
-        opcode <= "111111";
-        assertEqual(output, "0100000100");
-        wait for clk_period*1;
-        assertEqual(output, "0000000000");
-
-
-        -- Load upper immideate
-        reset <= '1';
-        wait for clk_period*1;
-        reset <= '0';
-        wait for clk_period*1;
-        assert (false) report "Testing load upper immideate" severity note;
-
-        -- Set instruction and wait for FETCH->EXECUTE
-        opcode <= "001111";
-        wait for clk_period*1;
-        -- Assert values and reset instruction to undefined
-        assertEqual(output, "0000000110");
-        wait for clk_period*1;
-        opcode <= "111111";
-        assertEqual(output, "0100000100");
-        wait for clk_period*1;
-        assertEqual(output, "0000000000");
-
-
-        -- Branch if equal
-        reset <= '1';
-        wait for clk_period*1;
-        reset <= '0';
-        wait for clk_period*1;
-        assert (false) report "Testing branch if equal" severity note;
-
-        -- Set instruction and wait for FETCH->EXECUTE
-        opcode <= "000100";
-        wait for clk_period*1;
-        -- Assert values and reset instruction to undefined
-        assertEqual(output, "0011000000");
-        wait for clk_period*1;
-        opcode <= "111111";
-        assertEqual(output, "0111000000");
-        wait for clk_period*1;
-        assertEqual(output, "0000000000");
-
-        -- Jump
-        reset <= '1';
-        wait for clk_period*1;
-        reset <= '0';
-        wait for clk_period*1;
-        assert (false) report "Testing jump" severity note;
-
-        -- Set instruction and wait for FETCH->EXECUTE
-        opcode <= "000010";
-        wait for clk_period*1;
-        -- Assert values and reset instruction to undefined
-        assertEqual(output, "0010000001");
-        wait for clk_period*1;
-        opcode <= "111111";
-        assertEqual(output, "0110000001");
-        wait for clk_period*1;
-        assertEqual(output, "0000000000");
-
-        -- Jump and link
-        reset <= '1';
-        wait for clk_period*1;
-        reset <= '0';
-        wait for clk_period*1;
-        assert (false) report "Testing jump and link" severity note;
-
-        -- Set instruction and wait for FETCH->EXECUTE
-        opcode <= "000011";
-        wait for clk_period;
-        -- Assert values and reset instruction to undefined
-        assertEqual(output, "1010000011");
-        wait for clk_period;
-        opcode <= "111111";
-        assertEqual(output, "1110000001");
-        wait for clk_period*1;
-        assertEqual(output, "0000000000");
-
-        wait;
-
-    end process;
+	end process;
 
 end;
