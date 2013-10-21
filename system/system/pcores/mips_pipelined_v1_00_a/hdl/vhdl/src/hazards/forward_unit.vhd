ae037c0f101f9e7b442ce82a6a2983e530b34130
Runar B. Olsen <runar.b.olsen@gmail.com>
2013-10-21 13:00:41 +0200

    Updated make file, fixed a few bugs in forwarding_unit and test bench

--- system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl/src/hazards/forward_unit.vhd
+++ system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl/src/hazards/forward_unit.vhd
@@ -10,33 +10,32 @@ entity forwarding_unit is
 		ex_mem_reg_write: in std_logic;
 		mem_wb_reg_write: in std_logic;
 		forwarding_A : out std_logic_vector (1 downto 0);
-		forwarding_B : out std_logic_vector (1 downto 0);
+		forwarding_B : out std_logic_vector (1 downto 0)
 	);
 end entity;
 
 architecture b of forwarding_unit is
 begin
-	process	(id_ex_register_rs, id_ex_register_rt, ex_mem_register_rd, mem_wb_register_wb, ex_mem_regwrite, mem_wb_regwrite)
+	process	(id_ex_register_rs, id_ex_register_rt, ex_mem_register_rd, mem_wb_register_rd, ex_mem_reg_write, mem_wb_reg_write)
 	begin  
 		-- type 1a hazard
 		if (ex_mem_reg_write = '1' AND (ex_mem_register_rd /= "00000") AND (ex_mem_register_rd = id_ex_register_rs)) then
-			forward_A <= "10";
+			forwarding_A <= "10";
 		-- type 2a hazard
-		elsif (mem_wb_reg_write	= '1' AND (mem_wb_register_rd /= "00000") AND NOT(ex_mem_reg_write AND (ex_mem_register_rd /= "00000")) 
-				AND (ex_mem_register_rd /= id_ex_register_rs) AND mem_wb_register_rd = id_ex_register_rs)) then 
-			forward_A <= "01";
+		elsif (mem_wb_reg_write	= '1' AND (mem_wb_register_rd /= "00000") AND (mem_wb_register_rd = id_ex_register_rs)) then 
+			forwarding_A <= "01";
 		else 
-			forward_A <= "00";
+			forwarding_A <= "00";
 		end if;
+		
 		-- type 1b hazard
-		if (ex_mem_reg_write = '1' AND (ex_mem_register_rd /= "00000") AND (ex_mem_register_rd = id_ex_register_rt)) then
-			forward_B <= "10";
+		if (ex_mem_reg_write = '1' AND ex_mem_register_rd /= "00000" AND ex_mem_register_rd = id_ex_register_rt) then
+			forwarding_B <= "10";
 		-- type 2b hazard
-		elsif (mem_wb_reg_write	= '1' AND (mem_wb_register_rd /= "00000") AND NOT(ex_mem_reg_write AND (ex_mem_register_rd /= "00000")) 
-				AND (ex_mem_register_rd /= id_ex_register_rt) AND mem_wb_register_rd = id_ex_register_rt)) then 
-			forward_B <= "01"; 
+		elsif (mem_wb_reg_write	= '1' AND (mem_wb_register_rd /= "00000") AND (mem_wb_register_rd = id_ex_register_rt)) then 
+			forwarding_B <= "01"; 
 		else 
-			forward_B <= "00";
+			forwarding_B <= "00";
 		end if;
 	end process;
 end architecture;
--- system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl/tests/hazards/forwarding_unit_tb.vhd
+++ system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl/tests/hazards/forwarding_unit_tb.vhd
@@ -1,55 +1,57 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+use work.asserts.all;
+
 entity forwarding_unit_tb is
 end forwarding_unit_tb;
 
-architecture Behavior of forwarding_unit is
+architecture Behavior of forwarding_unit_tb is
 	
 	-- Component Declaration for the Unit Under Test (UUT)
 	component forwarding_unit is
-	port (
-		id_ex_register_rs	: in std_logic_vector (4 downto 0);
-		id_ex_register_rt	: in std_logic_vector (4 downto 0);
-		ex_mem_register_rd	: in std_logic_vector (4 downto 0);
-		mem_wb_register_rd	: in std_logic_vector (4 downto 0);
-		ex_mem_reg_write	: in std_logic;
-		mem_wb_reg_write	: in std_logic;
-		forwarding_A 		: out std_logic_vector (1 downto 0);
-		forwarding_B 		: out std_logic_vector (1 downto 0);
-	);
+		port (
+			id_ex_register_rs	: in std_logic_vector (4 downto 0);
+			id_ex_register_rt	: in std_logic_vector (4 downto 0);
+			ex_mem_register_rd	: in std_logic_vector (4 downto 0);
+			mem_wb_register_rd	: in std_logic_vector (4 downto 0);
+			ex_mem_reg_write	: in std_logic;
+			mem_wb_reg_write	: in std_logic;
+			forwarding_A 		: out std_logic_vector (1 downto 0);
+			forwarding_B 		: out std_logic_vector (1 downto 0)
+		);
 	end component;
 	
-	   -- inputs
-		signal id_ex_register_rs  : in std_logic_vector (4 downto 0);
-		signal id_ex_register_rt  : in std_logic_vector (4 downto 0);
-		signal ex_mem_register_rd : in std_logic_vector (4 downto 0);
-		signal mem_wb_register_rd : in std_logic_vector (4 downto 0);
-		signal ex_mem_reg_write   : in std_logic;
-		signal mem_wb_reg_write   : in std_logic;
-		--outputs
-		signal forwarding_A 	  : out std_logic_vector (1 downto 0);
-		signal forwarding_B 	  : out std_logic_vector (1 downto 0);
-		
-	
+	-- inputs
+	signal id_ex_register_rs  : std_logic_vector (4 downto 0);
+	signal id_ex_register_rt  : std_logic_vector (4 downto 0);
+	signal ex_mem_register_rd : std_logic_vector (4 downto 0);
+	signal mem_wb_register_rd : std_logic_vector (4 downto 0);
+	signal ex_mem_reg_write   : std_logic;
+	signal mem_wb_reg_write   : std_logic;
+	--outputs
+	signal forwarding_A 	  : std_logic_vector (1 downto 0);
+	signal forwarding_B 	  : std_logic_vector (1 downto 0);
+			
 begin
 	
 	-- Instantiate the Unit Under Test (UUT)
 	uut: forwarding_unit 
 		port map (
-		id_ex_register_rs 	=> id_ex_register_rs,
-		id_ex_register_rt 	=> id_ex_register_rt,
-		ex_mem_register_rd  => ex_mem_register_rd,
-		mem_wb_register_rd	=> mem_wb_register_rd,
-		ex_mem_reg_write	=> ex_mem_reg_write,
-		mem_wb_reg_write	=> mem_wb_reg_write,
-		forwarding_A 		=> forwarding_A,
-		forwarding_B 		=> forwarding_B 
+			id_ex_register_rs 	=> id_ex_register_rs,
+			id_ex_register_rt 	=> id_ex_register_rt,
+			ex_mem_register_rd  => ex_mem_register_rd,
+			mem_wb_register_rd	=> mem_wb_register_rd,
+			ex_mem_reg_write	=> ex_mem_reg_write,
+			mem_wb_reg_write	=> mem_wb_reg_write,
+			forwarding_A 		=> forwarding_A,
+			forwarding_B 		=> forwarding_B 
 		);
 
 	stim_proc: process
 	begin        
 		wait for 10 ns;	
+		
 		-- testing forwarding_A = "00"
 		ex_mem_reg_write	<= '1';
 		mem_wb_reg_write	<= '1';
@@ -60,35 +62,78 @@ begin
 		wait for 10 ns;
 		assert(false) report "testing forwarding_A set to 00 " severity note;
 		assertEqual(forwarding_A, "00");
-		
-		-- testing forwarding_B = "00"
 		assert(false) report "testing forwarding_B set to 00 " severity note;
 		assertEqual(forwarding_B, "00");
 		
 		--testing forwarding_A = "10"
-		ex_mem_register_rd <= "00100";
+		ex_mem_reg_write	<= '1';
+		mem_wb_reg_write	<= '1';
+		id_ex_register_rs 	<= "00100";
+		id_ex_register_rt 	<= "00010";
+		ex_mem_register_rd  <= "00100";
+		mem_wb_register_rd	<= "00110";
+		wait for 10 ns;	
 		
-		wait for 10 ns;
 		assert(false) report "testing forwarding_A set to 10 " severity note;
 		assertEqual(forwarding_A, "10");
 		
+		-- Test forwarding_A = "10" if both stages write rd
+		ex_mem_reg_write	<= '1';
+		mem_wb_reg_write	<= '1';
+		id_ex_register_rs 	<= "00100";
+		id_ex_register_rt 	<= "00010";
+		ex_mem_register_rd  <= "00100";
+		mem_wb_register_rd	<= "00100";
+		wait for 10 ns;	
+		
+		assert(false) report "testing forwarding_A set to 10 when both stages write " severity note;
+		assertEqual(forwarding_A, "10");
+		
+		-- Test forwarding_B = "10" if both stages write rd
+		ex_mem_reg_write	<= '1';
+		mem_wb_reg_write	<= '1';
+		id_ex_register_rs 	<= "00100";
+		id_ex_register_rt 	<= "00010";
+		ex_mem_register_rd  <= "00010";
+		mem_wb_register_rd	<= "00010";
+		wait for 10 ns;	
+		
+		assert(false) report "testing forwarding_B set to 10 when both stages write " severity note;
+		assertEqual(forwarding_B, "10");
+		
 		-- testing forwarding_B = "10"
-		mem_wb_register_rd <= "00010"
+		ex_mem_reg_write	<= '1';
+		mem_wb_reg_write	<= '1';
+		id_ex_register_rs 	<= "00100";
+		id_ex_register_rt 	<= "00010";
+		ex_mem_register_rd  <= "00010";
+		mem_wb_register_rd	<= "00011";
 		wait for 10 ns;
+		
 		assert(false) report "testing forwarding_B set to 10 " severity note;
 		assertEqual(forwarding_B, "10");
 		
 		-- testing forwarding_A = "01"
-		ex_mem_reg_write = '0';
-		mem_wb_register_rd <= "00100";
-		
+		ex_mem_reg_write	<= '1';
+		mem_wb_reg_write	<= '1';
+		id_ex_register_rs 	<= "00100";
+		id_ex_register_rt 	<= "00010";
+		ex_mem_register_rd  <= "00110";
+		mem_wb_register_rd	<= "00100";
 		wait for 10 ns;
+		
 		assert(false) report "testing forwarding_A set to 01 " severity note;
 		assertEqual(forwarding_A, "01");
 		
 		-- testing forwarding_B = "01"
-		mem_wb_register_rd <= "00010";
+		ex_mem_reg_write	<= '1';
+		mem_wb_reg_write	<= '1';
+		id_ex_register_rs 	<= "00100";
+		id_ex_register_rt 	<= "00010";
+		ex_mem_register_rd  <= "00011";
+		mem_wb_register_rd	<= "00010";
 		wait for 10 ns;
+		
 		assert(false) report "testing forwarding_B set to 01 " severity note;
 		assertEqual(forwarding_B, "01");
 		
