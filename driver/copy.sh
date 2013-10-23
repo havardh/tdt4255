d8ec0ed49ba3c251aafa16704375b27409d2dda0
Runar B. Olsen <runar.b.olsen@gmail.com>
2013-10-23 10:07:02 +0200

    Updated copy script, it will now removed the correct file to cause a recompile of our pcore

--- driver/copy.sh
+++ driver/copy.sh
@@ -3,4 +3,7 @@
 find hardware/src -iname '*vhd*' -exec cp -v \{\} system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl/ \;
 cp -rv hardware/tests system/system/pcores/mips_pipelined_v1_00_a/hdl/vhdl
 
+# Remove implementation file if it exists so the processor will be recompiled
+rm -vf system/system/implementation/mips_pipelined_0_wrapper*
+
 echo "Successfully copied all files from hardware/ to pcores/"
